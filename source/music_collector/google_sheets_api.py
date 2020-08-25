bc3650a80fe690805f900f3fb39cd64fe09fdb54
Andrei Khodko <andrei.khodko@gmail.com>
2020-08-25 12:59:03 +0500

    Sourse folder created

--- source/music_collector/google_sheets_api.py
+++ source/music_collector/google_sheets_api.py
@@ -4,7 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from secretConfig import gsheets_settings
+from source.secretConfig import gsheets_settings
 from enum import Enum
 
 
