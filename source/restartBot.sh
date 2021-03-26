9d89e31a39442967d29363f25a0892cf16bb4061
Andrei Khodko <andrei.khodko@gmail.com>
2021-03-27 02:02:09 +0300

    WTF with this source

--- source/restartBot.sh
+++ source/restartBot.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 killall python
-. /root/venv/bin/activate
+source /root/venv/bin/activate
 pip install --upgrade -r requirements.txt
 nohup python "$(dirname "$0")/main.py" 1> bot.out 2> bot.err &
